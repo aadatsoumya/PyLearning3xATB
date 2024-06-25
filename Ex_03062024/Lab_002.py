0f8bae4f44a3c2fdf787e2db26841ebf9c8fc62a
Ssahu4 <aadatsoumya@gmail.com>
2024-06-25 18:03:12 +0530

    Adding Lab_002,Lab_003,Lab_004 file

--- Ex_03062024/Lab_002.py
+++ Ex_03062024/Lab_002.py
@@ -1,1 +1,3 @@
 print("Lab_002 file")
+print("Lab_002 file")
+print(5,2)
---
+++ Ex_03062024/Lab_004.py
@@ -0,0 +1,3 @@
+num=int(input("Enter any number:"))
+for x in range(1, 11,1):
+ print( str(num) +" * " +str(x) +" = "+ str(num*x))
